d1cdac4afe3ce5452fdb6da60d2d18c5a1e82565
bhargav pj <115325595+name-is-pj@users.noreply.github.com>
2026-09-25 01:05:54 +0530

    Make v18 source buildable

--- gaming_zone.py
+++ gaming_zone.py
@@ -1,8 +1,11 @@
-import base64, gzip
+import base64
+import gzip
 
-_B64 = """
-LOAD_FROM_B64_FILE
-"""
+from source_parts.v18_01 import PART as P1
+from source_parts.v18_02 import PART as P2
+from source_parts.v18_03 import PART as P3
+from source_parts.v18_04 import PART as P4
+from source_parts.v18_05 import PART as P5
 
-source = gzip.decompress(base64.b64decode(_B64)).decode("utf-8")
-exec(compile(source, __file__, "exec"), globals(), globals())
+_SOURCE = gzip.decompress(base64.b64decode(P1 + P2 + P3 + P4 + P5)).decode("utf-8")
+exec(compile(_SOURCE, __file__, "exec"), globals(), globals())
